82232aac8a034849ce9e9ea9d812946a08ceb0da
Geo Perez <geovanni.perez@gmail.com>
2015-10-14 13:34:03 -0500

    Updating first test

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Unosquare.Labs.EmbedIO;
+using Unosquare.Labs.EmbedIO.Log;
+using Unosquare.Labs.EmbedIO.Modules;
+
+namespace unopi
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var url = "http://localhost:9696/";
+            if (args.Length > 0)
+                url = args[0];
+
+            // Our web server is disposable. Note that if you don't want to use logging,
+            // there are alternate constructors that allow you to skip specifying an ILog object.
+            using (var server = new WebServer(url, new SimpleConsoleLog()))
+            {
+                // First, we will configure our web server by adding Modules.
+                // Please note that order DOES matter.
+                // ================================================================================================
+                // If we want to enable sessions, we simply register the LocalSessionModule
+                // Beware that this is an in-memory session storage mechanism so, avoid storing very large objects.
+                // You can use the server.GetSession() method to get the SessionInfo object and manupulate it.
+                // You could potentially implement a distributed session module using something like Redis
+                server.RegisterModule(new LocalSessionModule());
+
+                // Here we setup serving of static files
+                server.RegisterModule(new StaticFilesModule("/home/pi/target"));
+                // The static files module will cache small files in ram until it detects they have been modified.
+                server.Module<StaticFilesModule>().UseRamCache = true;
+                server.Module<StaticFilesModule>().DefaultExtension = ".html";
+                // We don't need to add the line below. The default document is always index.html.
+                //server.Module<Modules.StaticFilesWebModule>().DefaultDocument = "index.html";
+
+                // Once we've registered our modules and configured them, we call the RunAsync() method.
+                // This is a non-blocking method (it return immediately) so in this case we avoid
+                // disposing of the object until a key is pressed.
+                //server.Run();
+                server.RunAsync();
+
+                // Fire up the browser to show the content if we are debugging!
+#if DEBUG
+                var browser = new System.Diagnostics.Process()
+                {
+                    StartInfo = new System.Diagnostics.ProcessStartInfo(url) {UseShellExecute = true}
+                };
+                browser.Start();
+#endif
+                // Wait for any key to be pressed before disposing of our web server.
+                // In a service we'd manage the lifecycle of of our web server using
+                // something like a BackgroundWorker or a ManualResetEvent.
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
